d7dacd83b2b8c7ea2d23bb34d42d930030394288
ANTON ZAHARIA <zaharia_anton@yahoo.com>
2020-04-04 13:58:02 +0000

    Automatically backed up by Learn

--- app/models/character.rb
+++ app/models/character.rb
@@ -15,7 +15,7 @@ class Character < ActiveRecord::Base
   def build_network(hash={})
     net = Network.new
     net.call_letters = hash.fetch(:call_letters)
-    self.show = net
+    self.show.network = net
   end
 
 end
